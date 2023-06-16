a7a43bf2caa9c26e85dbb37cf2743e25d32e1360
JonathanF <jonathan.farrell2@ucdconnect.ie>
2023-06-16 10:44:02 +0100

    initial commit

---
+++ src/test/java/app/planner/application/PlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package app.planner.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
