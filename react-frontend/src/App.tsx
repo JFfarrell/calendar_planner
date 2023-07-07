1dbaaed21e4ab95d53ea5e69e57a56a0e677d5ae
JonathanF <jonathan.farrell2@ucdconnect.ie>
2023-07-07 23:14:32 +0100

    addition of basic front-end drop down. no backend functionality attached yet

--- react-frontend/src/App.tsx
+++ react-frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import activityList from "./backend-connection/PresentActivities";
 import ActivityList from "./backend-connection/PresentActivities";
+import ActivityForm from "./backend-connection/ActivityForm";
 
 function App() {
   return (
     <div>
       <ActivityList></ActivityList>
+      <ActivityForm onSubmit={(data) => console.log(data)}></ActivityForm>
     </div>
   );
 }
--- react-frontend/src/Schemas.tsx
+++ react-frontend/src/Schemas.tsx
@@ -1,27 +1,27 @@
 import React from "react";
-import { z } from 'zod';
+import { z } from "zod";
 
-export const categories = ['Groceries', 'Utilities', 'Entertainment'] as const;
+export const categories = ["Run", "Bike", "Swim", "Other"] as const;
 export const errorStrings = {
-    minDescriptionLength: 'Description must be at least 3 characters',
-    maxDescriptionLength: 'Description must be less than 50 characters',
-    amountIsRequired: "Amount is required",
-    amountValue: 'Amount must be between 0.01 and 100,000',
-    permittedCategories: 'Please select a category',
+  minDescriptionLength: "Description must be at least 3 characters",
+  maxDescriptionLength: "Description must be less than 50 characters",
+  amountIsRequired: "Amount is required",
+  amountValue: "Amount must be between 1 and 180",
+  permittedCategories: "Please select a category",
 };
 
 export const schema = z.object({
-    descrption: z
+  description: z
     .string()
-    .min(3, { message: errorStrings.minDescriptionLength})
-    .max(50, { message: errorStrings.maxDescriptionLength}),
-    amount: z
+    .min(3, { message: errorStrings.minDescriptionLength })
+    .max(50, { message: errorStrings.maxDescriptionLength }),
+  activity_duration: z
     .number()
-    .min(0.01, { message: errorStrings.amountValue})
-    .max(100_000, { message: errorStrings.amountValue}),
-    category: z.enum(categories, {
-        errorMap: () => ({ message: 'Category is required'}),
-    })
-})
+    .min(1, { message: errorStrings.amountValue })
+    .max(180, { message: errorStrings.amountValue }),
+  category: z.enum(categories, {
+    errorMap: () => ({ message: "Category is required" }),
+  }),
+});
 
 export type ExpenseFormData = z.infer<typeof schema>;
